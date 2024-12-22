69fc8553500bc9660ef27a431eea435f5a02dca3
Kajal <kajalgiri16@gmail.com>
2024-12-21 21:44:37 -0500

    Updated components of product gridwall & navbar.

---
+++ src/AboutUs.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./AboutUs.css";
+import { useNavigate } from "react-router-dom";
+
+function AboutUs() {
+  const navigate = useNavigate();
+
+  const handleGetStartedClick = () => {
+    navigate("/products");
+  };
+
+  return (
+    <div className="landing-page">
+      <div className="background-image"></div>
+      <div className="about-container">
+        <div className="about-details">
+          <h1>Paradise Nursery</h1>
+          <div className="divider"></div>
+          <p>Where Every Plants Tells a Story!</p>
+
+          <button className="get-started-btn" onClick={handleGetStartedClick}>
+            Get Started
+          </button>
+        </div>
+        <div className="content">
+          <p className="about-us-details">
+            Paradise Nursery: Where Every Plant Tells a Story!
+          </p>
+          <p className="about-us-content">
+            At Paradise Nursery, we believe in the magic of greenery and its
+            power to transform spaces and lives. Our nursery is more than just a
+            place to buy plants—it’s a haven for nature lovers, a sanctuary for
+            those seeking calm, and a hub of inspiration for aspiring gardeners.
+            From vibrant flowers that brighten your home to low-maintenance
+            succulents perfect for busy lifestyles, we offer a thoughtfully
+            curated collection for every need.
+          </p>
+          <p className="about-us-content">
+            We source our plants from trusted growers, ensuring they are
+            healthy, sustainable, and nurtured with care. Whether you're a
+            seasoned gardener or just starting your journey, our friendly team
+            is here to guide you with expert advice and creative ideas.
+          </p>
+          <p className="about-us-content">
+            At Paradise Nursery, each plant has a story, and we invite you to
+            make it part of yours. Let us help you bring a touch of nature into
+            your life and create your own little paradise. Together, let’s grow,
+            blossom, and thrive!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
---
+++ src/Navbar.js
@@ -0,0 +1,27 @@
+import "./Navbar.css";
+import logoImg from "./images/logo.png";
+import cartImg from "./images/cart.png";
+
+function Navbar() {
+  return (
+    <div className="nav-bar">
+      <div className="logo">
+        <img className="logo-image" src={logoImg} alt="logo-Image" />
+      </div>
+      <div className="logo-text">
+        <h1>Paradise Nursery</h1>
+        <p>
+          <i>Where Every Plants Tells a Story!</i>
+        </p>
+      </div>
+      <div className="header-text">
+        <h2>Plants</h2>
+      </div>
+      <div className="cart">
+        <img className="cart-image" src={cartImg} alt="cart-Image" />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
